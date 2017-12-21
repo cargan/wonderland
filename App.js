d6b6f9e71aa4ce7c651c00df510a37e057f87984
LInas <linas@localhost.localdomain>
2017-12-21 20:36:25 +0200

    colors and blinking

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const COLORS = [
+  'gray', 'red', 'olive', 'blue', 'orange', 'yellow'
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: 'gray',
+      timer: null
+    };
+  }
+
+  componentWillUnmount() {
+    this.clearInterval(this.state.timer);
+  }
+
+  onColorClick() {
+    const idx = COLORS.indexOf(this.state.bgColor);
+    if (idx !== COLORS.length - 1) {
+      this.setState({'bgColor': COLORS[(idx+1)]});
+    } else {
+      this.setState({'bgColor': COLORS[0]});
+    }
+  }
+
+  onBlinkClick() {
+    if (!this.state.timer) {
+      this.setState({
+        timer: setInterval(this.onColorClick.bind(this), 750)
+      });
+    } else {
+      clearInterval(this.state.timer);
+      this.setState({timer: null});
+    }
+  }
+
   render() {
+    const appClasses = 'App ' + this.state.bgColor;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={appClasses}>
+        <ul className='navigation'>
+          <li onClick={() => this.onColorClick()}><a>Change Color</a></li>
+          <li onClick={() => this.onBlinkClick()}><a>Blink</a></li>
+        </ul>
       </div>
     );
   }
